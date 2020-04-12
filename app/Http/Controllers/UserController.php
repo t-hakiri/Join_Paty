5ac568f917b44e6d47055eb833fb9297a4ea4761
takuya hakiri <tyapi4@yahoo.co.jp>
2020-04-12 19:32:51 +0900

    user show new

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
@@ -12,4 +13,20 @@ class UserController extends Controller
     // ->except・・・認証を除外するアクション配列
     // ->only・・・認証が必要なアクション配列
   }
+
+    public function show($id)
+    {
+      $user = User::findOrFail($id);
+
+      return view('users/show', compact('user'));
+    }
+
+  public function edit($id)
+    {
+      // DBよりURIパラメータと同じIDを持つUserの情報を取得
+      $user = User::findOrFail($id);
+
+      // 取得した値をビュー「user/edit」に渡す
+      return view('user/edit', compact('user'));
+    }
 }
