63f898c37b1f23e133db8b21f8c964aab0a5b61d
takuya hakiri <tyapi4@yahoo.co.jp>
2020-04-19 14:38:37 +0900

    [add]chatにリンク追加

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -1,10 +1,12 @@
 <div class="media">
     <div class="media-body comment-body">
         <div class="row">
-            <span class="comment-body-user">{{optional($message->users->first())->name}}</span>
+            <a class="comment-body-user" href="{{ action('UserController@show', optional($message->users->first())->id) }}">{{optional($message->users->first())->name}}</a>
             <span class="comment-body-time">{{$message->created_at}}</span>
         </div>
         <span class="comment-body-content">{{$message->body}}</span>
     </div>
 </div>
+
+
 
